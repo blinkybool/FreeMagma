72848aaf9cafc61e0756fd0b28dab277983540b7
Billy <billyprice98is@gmail.com>
2020-01-20 11:22:55 +1100

    improved magma.js, switched mountain orientation

--- magma.js
+++ magma.js
@@ -1,7 +1,4 @@
 class FreeMagma {
-	constructor(signature) {
-		this.signature = signature;
-	}
 
 	static factorise(_) {
 		throw new Error('factorise method not implemented!');
@@ -10,6 +7,12 @@ class FreeMagma {
 	static product({ funcID, args }) {
 		return this.operators[args.length][funcID](...args);
 	}
+
+	static converter(Domain, Codomain) {
+		return (x) => Codomain.product(Domain.factorise(x));
+	}
+
+	static identity = (Domain) => this.converter(Domain, Domain);
 }
 
 class MagmaProduct {
@@ -64,6 +67,33 @@ class Catalan extends FreeMagma {
 	static catalan_objects(n) {
 		return this.catalan_products(n).map((currentValue) => this.product(currentValue));
 	}
+
+	static infix_rep(magma_product) {
+		if (magma_product.is_generator()) {
+			return 'o'; 
+		} else {
+			let {args: [first, second]} = magma_product;
+			return '(' + this.infix_rep(first) + '*' + this.infix_rep(second) + ')';
+		}
+	}
+
+	static prefix_rep(magma_product) {
+		if (magma_product.is_generator()) {
+			return 'o'; 
+		} else {
+			let {args: [first, second]} = magma_product;
+			return '*' + this.prefix_rep(first) + this.prefix_rep(second);
+		}
+	}
+
+	static postfix_rep(magma_product) {
+		if (magma_product.is_generator()) {
+			return 'o'; 
+		} else {
+			let {args: [first, second]} = magma_product;
+			return this.postfix_rep(first) + this.postfix_rep(second) + '*';
+		}
+	}
 }
 
 class Brackets extends Catalan {
@@ -87,23 +117,75 @@ class Brackets extends Catalan {
 		let second = this.factorise(brackets.slice(i + 1, -1));
 		return new UniqueProduct(first, second);
 	}
+
 }
 
 class Mountains extends Catalan {
 	static generator = () => '';
-	static binary_op(first, second) {
+	static binary_op_vert(first, second) {
 		const separator = (first.is_generator() ? '' : '\n');
 
-		return second.is_generator()
-			? Mountains.product(first) + separator + '\\\n/'
+		return second.is_generator() ?
+			Mountains.product(first) + separator + '\\\n/'
 			: Mountains.product(first) + separator +
 					'\\\n ' +
 					Mountains.product(second).replace(/\n/g, '\n ') +
 					'\n/';
 	}
-	static operators = [ [ this.generator ], undefined, [ this.binary_op ] ];
+	static binary_op_hori = (first, second) => {
+		const first_rows = this.product(first).split('\n');
+		let second_rows = this.product(second).split('\n');
+
+		if (second.is_generator()) {
+			second_rows = [ '/\\' ];
+		} else {
+			second_rows = second_rows.map((currentValue) => ' ' + currentValue + ' ');
+			second_rows.push('/' + ' '.repeat(second_rows[0].length - 2) + '\\');
+		}
+
+		const height = Math.max(first_rows.length, second_rows.length);
+
+		for (let piece_rows of [first_rows, second_rows]) {
+			const row_fill = ' '.repeat(piece_rows[0].length);
+			const num_padding_rows = height - piece_rows.length
+			piece_rows.unshift(...Array(num_padding_rows).fill(row_fill));
+		}
+
+		let result_rows = [];
+		for (let i=0; i<height; ++i) {
+			result_rows.push(first_rows[i] + second_rows[i]);
+		}
+
+		return result_rows.join('\n');
+	}
+	
+	static operators = [ [ this.generator ], undefined, [ this.binary_op_hori ] ];
 
 	static factorise(mountain) {
+		if (mountain == '') return new UniqueGenerator();
+
+		const rows = mountain.split('\n');
+
+		const split_index = rows[rows.length-1].lastIndexOf('/');
+		const first_rows = [];
+		const second_rows = [];
+
+		for (let row of rows) {
+			first_rows.push(row.slice(0,split_index));
+			second_rows.push(row.slice(split_index+1, row.length-1));
+		}
+		second_rows.pop();
+
+		for (let piece_rows of [ first_rows, second_rows]) {
+			// Remove whitespace rows
+			while (piece_rows.length > 0 && /^\s*$/.test(piece_rows[0])) piece_rows.shift();
+		}
+
+		return new UniqueProduct(this.factorise(first_rows.join('\n')),
+														 this.factorise(second_rows.join('\n')));
+	}
+
+	static factorise_vert(mountain) {
 		if (mountain == '') return new UniqueGenerator();
 
 		const last_off_diag = mountain.lastIndexOf('\n\\') + 1;
@@ -114,12 +196,15 @@ class Mountains extends Catalan {
 	}
 }
 
-for (i = 0; i <= 5; ++i) {
+for (let i = 0; i <= 5; ++i) {
 	console.log('Catalan Size: ' + i);
 	console.log('----------------');
 	for (let catalan_product of Catalan.catalan_products(i)) {
+		let mountain = Mountains.product(catalan_product);
+		// console.log(Mountains.product(Mountains.factorise(mountain)));
+		console.log(mountain);
 		console.log(Brackets.product(catalan_product));
-		console.log(Mountains.product(catalan_product));
+		
+		console.log('-'.repeat(i*2));
   }
 }
-
