0fa5c547ee7784e3292e500975650217d01b8a63
William Price <wprice@6200D-139450-M.local>
2020-01-20 16:25:30 +1100

    implemented magma.py with tests

---
+++ tests/test_magma.py
@@ -0,0 +1,18 @@
+import unittest
+from magma import Brackets, Mountains
+
+class TestBrackets(unittest.TestCase):
+
+  def test_identity(self):
+    brackets = '()()((()((()))))'
+    self.assertEqual(Brackets.identity()(brackets), brackets)
+
+class TestMountains(unittest.TestCase):
+
+  def test_identity(self):
+    mountain = '\n'.join(
+                        [r'     /\       ',
+                         r'    /  \/\    ',
+                         r' /\/      \   ',
+                         r'/          \/' + '\\']) # can't end raw-string literal in a backslash
+    self.assertEqual(Mountains.identity()(mountain), mountain)
