5a5f61b3f8d67f6e0116129c2858bd708a4faf87
Billy-imac-desktop <billyprice98is@gmail.com>
2020-02-11 13:29:44 +1100

    developing staircase Polygons tikz

--- magma/tikz.py
+++ magma/tikz.py
@@ -7,3 +7,18 @@ def wrap_tikz_env(env_name, content):
 def wrap_tikz_command(command_name, *args, optional=None):
   optional = f'[{optional}]' if optional is not None else ''
   return '\\' + str(command_name) + optional +  ''.join(f'{{{arg}}}' for arg in args)
+
+def embed_lattice_path(steps, step_dir, start=(0,0), with_start=False):
+  (x,y) = start
+
+  if with_start:
+    yield (x,y)
+  
+  for step in steps:
+    dx, dy = step_dir[step]
+
+    x += dx
+    y += dy
+
+    yield (x,y)
+  
